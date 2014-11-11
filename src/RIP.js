7e8e8307010aab4e4a75280f2a1e07a0116d85c9
pyrsmk <dev@dreamysource.fr>
2014-11-11 12:15:03 +0100

    Update AMD declaration

--- src/RIP.js
+++ src/RIP.js
@@ -7,13 +7,13 @@
 
 ;(function(context,name,definition){
 	if(typeof module!='undefined' && module.exports){
-		module.exports=definition();
+		module.exports=definition;
 	}
 	else if(typeof define=='function' && define.amd){
 		define(definition);
 	}
 	else{
-		context[name]=definition();
+		context[name]=definition;
 	}
 }(this,'RIP',function(){
 
@@ -125,4 +125,4 @@
 
 	};
 	
-}));
+}()));
